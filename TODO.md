c992b0919bf89ec89452eb8af1514d0bdf720caf
Erik Vinblad <erik.winbladh14@gmail.com>
2023-04-16 20:20:50 +0200

    Add error handling to pre_processing.py

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO List
+
+- Initialize database to hold already queried video information (implement a TTL on each stored request).
--- pre_processing.py
+++ pre_processing.py
@@ -1,28 +1,29 @@
 from bs4 import BeautifulSoup
+import os
 
 def extract_video_ids(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
     with open(file_path, 'r', encoding='utf-8') as file:
         soup = BeautifulSoup(file.read(), 'lxml')
 
-    video_ids = []
-    for idx, link in enumerate(soup.find_all('a')):
-        url = link.get('href')
-        if 'watch?v=' in url:
-            video_id = url.split('watch?v=')[1].split('&')[0]
-            video_ids.append(video_id)
+    video_ids = [link.get('href').split('watch?v=')[1].split('&')[0] for link in soup.find_all('a') if 'watch?v=' in link.get('href', '')]
 
     print(f"Videos found in watch history: {len(video_ids)}")
     return video_ids
 
 def save_video_ids(video_ids, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
-        for video_id in video_ids:
-            file.write(f"{video_id}\n")
+        file.writelines(f"{video_id}\n" for video_id in video_ids)
 
 def preprocess_watch_history(input_file='history/watch-history.html', output_file='history/extracted_video_ids.txt'):
-    print("Extracting video IDs from Google Takeout watch history...")
-    video_ids = extract_video_ids(input_file)
-    save_video_ids(video_ids, output_file)
+    try:
+        print("Extracting video IDs from Google Takeout watch history...")
+        video_ids = extract_video_ids(input_file)
+        save_video_ids(video_ids, output_file)
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     preprocess_watch_history()
